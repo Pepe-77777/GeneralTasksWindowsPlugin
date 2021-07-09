31861b1a369f7d888b0212d8216469823817ffcf
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-07-09 09:39:36 -0300

    Add files via upload

--- general-tasks.bat
+++ general-tasks.bat
@@ -5,30 +5,36 @@ if [%1]==[] goto notArgs
 if [%1]==[read] goto read
 if [%1]==[open] goto open
 if [%1]==[github] goto github
+if [%1]==[senhas] goto senhas
 
 :read
 cls
-cd Assets
+cd Assets_GeneralTasks
 start Read.bat
 exit
 
 :open
 cls
-cd Assets
+cd Assets_GeneralTasks
 start Open.bat
 exit
 
 :github
 cls
-cd Assets
+cd Assets_GeneralTasks
 start Github.bat
 exit
+
+:senhas
+cls
+cd Assets_GeneralTasks
 
 :notArgs
 cls
 echo Seja bem vindo ao general tasks, para comecar digite:
 echo General-Tasks read (Leia um arquivo usando o cmd)
 echo General-Tasks open (Abrir um arquivo)
-echo General-Tasks github (Abrir um arquivo)
+echo General-Tasks github (Abra a pagina do GITHUB.)
+echo General-Tasks senhas (Salvar uma senha)
 
 title
